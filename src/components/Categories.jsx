c057dd6879152e623121355556486fad843dede0
Andrey <an.schilow@yandex.ru>
2021-12-03 09:17:14 +0300

    v2

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -2,6 +2,37 @@ import React from 'react'
 
 
 
+
+
+export default 
+
+
+function Categories({items, onClickItem }) {
+    const [activeItem, setActiveItem] = React.useState(null);
+    
+
+    const onSelectItem = (index) => {
+        setActiveItem(index);
+           };
+   
+    return (
+        <div>
+              <div className="categories">
+            <ul>         
+              <li  className={activeItem === null ? 'active' : '' } onClick={() => onSelectItem(null)}>
+              Все
+              </li>
+              {items && items.map((name, index) => ( // Итерация по массиву с категориями 
+                  <li 
+                  className={activeItem === index ? 'active' : '' }
+                  onClick={() => onSelectItem(index)} key={`${name}_${index}`}> {name}</li>)// Более уникальное значение <li> key={`${name}_${index}`}>
+               ) }
+             
+            </ul>
+          </div>
+        </div>
+    )
+}
 // class Categories extends React.Component { 
 //     state = {
 //         activeItem: 1,
@@ -40,38 +71,4 @@ import React from 'react'
 //             }
 //         }
 
-
-
 // export default Categories;
-
-
-
-export default 
-
-
-function Categories({items, onClickItem }) {
-    const [activeItem, setActiveItem] = React.useState(null);
-    
-
-    const onSelectItem = (index) => {
-        setActiveItem(index);
-           };
-   
-    return (
-        <div>
-              <div className="categories">
-            <ul>         
-              <li  className={activeItem === null ? 'active' : '' } onClick={() => onSelectItem(null)}>
-              Все
-              </li>
-              {items && items.map((name, index) => ( // Итерация по массиву с категориями 
-                  <li 
-                  className={activeItem === index ? 'active' : '' }
-                  onClick={() => onSelectItem(index)} key={`${name}_${index}`}> {name}</li>)// Более уникальное значение <li> key={`${name}_${index}`}>
-               ) }
-             
-            </ul>
-          </div>
-        </div>
-    )
-}
