e661fab9c197af40f144b0937991c5211bf20184
Andrey <an.schilow@yandex.ru>
2022-01-23 23:40:39 +0300

    Online-cosmetics-store

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import logoSvg from "../assets/img/risunok1.svg";
+import logoSvg from "../assets/img/risunok4.svg";
 import Button from "./Button";
 
 // Рендер Шапки 
